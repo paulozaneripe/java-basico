6d172b55868c8c1e84042b175ad3b5614f2d4627
Samara Kristiny Souza Gomes de Castro <sa_gomes@live.com>
2021-07-20 14:39:31 -0300

    Soma e multiplicacao em um vetor

---
+++ Programa.java
@@ -0,0 +1,34 @@
+import java.util.Objects;
+
+public class Programa {
+    
+    public static void main(String[] args_) {
+        String[] args = {"multiplica","1","2","5","8","9"};
+
+        if(Objects.nonNull(args) && args.length > 0) {
+            if(args[0] == "soma" && args.length > 1) {
+                Integer soma = 0;
+
+                for(int i = 1; i < args.length; i++) {
+                    soma = soma + Integer.valueOf(args[i]);
+                }
+
+                System.out.println("Resultado da soma: " + soma);
+
+            } else if(args[0] == "multiplica" && args.length > 1) {
+                Integer multiplicacao = 1;
+                
+                for(int i = 1; i < args.length; i++) {
+                    multiplicacao = multiplicacao * Integer.valueOf(args[i]);
+                }
+
+                System.out.println("Resultado da multiplicação: " + multiplicacao);
+            } else {
+                System.out.println("A operação '" + args[0] + "' não existe!");
+            }
+        } else {
+            System.out.println("Vetor vazio!");
+        }
+
+    }
+}
